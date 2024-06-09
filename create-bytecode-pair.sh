e180812bb74990ce3496a4e28e902377d0240c8e
eugenioclrc <eugenioclrc@gmail.com>
2024-06-09 20:38:16 -0300

    Factory is ready :D

--- create-bytecode-pair.sh
+++ create-bytecode-pair.sh
@@ -1,6 +1,3 @@
-echo -n "hex\"" > pair.bytecode
+echo 'hex"' > pair.bytecode
 huffc -e paris -b --optimize src/LPToken.huff >> pair.bytecode
-echo -n "\"" >> pair.bytecode
-sed -i "s/c00ffec00ffec00ffec00ffec00ffec00ffec00f/\",abi.encode(address(this)),hex\"/g" pair.bytecode
-sed -i "s/beefbeefbeefbeefbeefbeefbeefbeefbeefbeef/\",abi.encode(token0),hex\"/g" pair.bytecode
-sed -i "s/c0dec0dec0dec0dec0dec0dec0dec0dec0dec0de/\",abi.encode(token1),hex\"/g" pair.bytecode
+echo '"' >> pair.bytecode
