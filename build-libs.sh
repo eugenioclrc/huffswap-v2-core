858c8a7c4e1c608c1d9f008351a9feaef61dc00a
Eugenio <eugenioclrc@gmail.com>
2024-06-24 23:18:16 -0300

    fmt

--- build-libs.sh
+++ build-libs.sh
@@ -1,3 +1,3 @@
 balls src/libraries/Hash.balls -d --output-path src/libraries/Hash.huff
 balls src/libraries/SafeERC20.balls -d --output-path src/libraries/SafeERC20.huff
-balls  src/libraries/SafeMath.balls -d --output-path  src/libraries/SafeMath.huff
+balls src/libraries/SafeMath.balls -d --output-path  src/libraries/SafeMath.huff
